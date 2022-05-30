311dd56f1f5893c4b6306c2483aa06af4e7dd039
with-developer <miki3079@naver.com>
2022-05-30 23:47:54 +0900

    menu export text

--- Restaurant/test.py
+++ Restaurant/test.py
@@ -1,5 +1,6 @@
-import openpyxl
-wb=openpyxl.load_workbook('/Users/junja/Desktop/python/Restaurant/example.xlsx') #파일명 혹은 패스+파일명 
+import random
 
-test=wb.get_sheet_names()
-print(test)
+
+A_restaurant = (("menu 1-1", 1000),("menu 1-2", 2000))
+
+print(A_restaurant[0][0])
