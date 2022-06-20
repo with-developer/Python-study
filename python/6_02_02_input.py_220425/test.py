8258deb93c6d3a1f15065b47700770c897f1212a
with-developer <miki3079@naver.com>
2022-06-21 00:09:24 +0900

    update

--- python/6_02_02_input.py_220425/test.py
+++ python/6_02_02_input.py_220425/test.py
@@ -1,8 +1,15 @@
-result = 0
-i = 100
+import random
 
-for i in range(102,201,3):
-    result += i
+max_num = -1
+for i in range (0, 100) :
+    random_num = random.randrange(0, 1000)
+    if random_num > max_num :
+        max_num = random_num
 
+print("가장 큰 수는 : ", max_num)
 
-print(result)
+min_num = 1000
+for i in range (0, 100) :
+    random_num = random.randrange(0, 1000)
+    if min_num > random_num :
+        min_num = random_num
